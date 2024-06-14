67f5a2a3c0d1f0e4087b50b2fe94effc37dfb591
kittipat <kittipatkimmy0524@gmail.com>
2024-06-14 20:38:49 +0700

    []

---
+++ ctp_hr_recruitment_custom/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Recruitment Customs",
+    "version": "17.0.1.0.1",
+    "description": "Recruitment Customizations",
+    "author": "Cybernetics+",
+    "website": "https://www.cybernetics.plus",
+    "live_test_url": "https://www.cybernetics.plus",
+    "category": "Customizations",
+    "license": "Other proprietary",
+    "price": 999999999.99,
+    "currency": "EUR",
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "contributors": [
+        "C+ Developer <dev@cybernetics.plus>",
+    ],
+    "depends": ["base", "hr", "hr_recruitment"],
+    "data": [
+        'views/hr_employee_view.xml',
+        'views/hr_applicant_view.xml',
+    ],
+}
---
+++ ctp_hr_recruitment_custom/models/hr_applicant.py
@@ -0,0 +1,17 @@
+from odoo import models, fields
+
+
+class Applicant(models.Model):
+    _inherit = "hr.applicant"
+
+    name_ch = fields.Char()
+
+    def _get_employee_create_vals(self):
+        self.ensure_one()
+        res = super(Applicant, self)._get_employee_create_vals()
+        if 'name_ch' not in res:
+            res.update({
+                'name_ch': self.name_ch if self.name_ch else ''
+            })
+
+        return res
